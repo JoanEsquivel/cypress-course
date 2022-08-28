6beaa031200ac55aaab27c13fdeddc2b5701e549
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-08-28 12:17:32 -0700

    Adding basic api test ex[planation

--- cypress/e2e/39-api-testing-demo.cy.ts
+++ cypress/e2e/39-api-testing-demo.cy.ts
@@ -1,3 +1,42 @@
+describe("Basic API test examples", () => {
+  it("GET request", () => {
+    cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1").then(
+      (response) => {
+        cy.log("response", response);
+      }
+    );
+  });
+
+  it("POST a new resource", () => {
+    cy.request("POST", "https://jsonplaceholder.typicode.com/posts", {
+      title: "title #101",
+      body: "post #101",
+      userId: 101,
+    }).then((response) => {
+      cy.log("response", response);
+    });
+  });
+
+  it("Update a resource", () => {
+    cy.request("PUT", "https://jsonplaceholder.typicode.com/posts/1", {
+      id: 1,
+      title: "title example",
+      body: "body example",
+      userId: 1,
+    }).then((response) => {
+      cy.log("response", response);
+    });
+  });
+
+  it("Delete a resource", () => {
+    cy.request("DELETE", "https://jsonplaceholder.typicode.com/posts/1").then(
+      (response) => {
+        cy.log("response", response);
+      }
+    );
+  });
+});
+
 describe("API Testing Demo", () => {
   it("Creating a user(emulation)", function () {
     const userId: string = "7d01de84-9527-4855-a10c-043a637178b3";
