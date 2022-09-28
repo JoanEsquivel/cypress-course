5c9981a4eb690ec25968a5178410aafc07e6dce4
JoanEsquivel <joan.esquim@gmail.com>
2022-09-28 14:39:14 -0700

    cypress 10.9.0

--- cypress/e2e/40-hide-sensitive-data.cy.ts
+++ cypress/e2e/40-hide-sensitive-data.cy.ts
@@ -3,18 +3,18 @@ describe("Hidding username and password to avoid data leaks", () => {
     cy.visit(`${Cypress.env("demoQA")}/login`);
   });
   it("Valid Login", () => {
-
     //1- Get the sensitive data from environment variables
     const username = Cypress.env("user");
-    //To set the password use: "CYPRESS_password=test1234* npx cypress open" so you set the environment variable at the execution time
+    //To set the password use:
+    //BASH: export CYPRESS_password=test1234*
     //You can use the package "as a" https://github.com/bahmutov/as-a from Glen Bahmutov to create a safer way to declare environment variables
     const password = Cypress.env("password");
 
     //2- Make sure the data was set with pre-assertions
-    expect(username, 'username was set').to.be.a('string').and.not.be.empty
+    expect(username, "username was set").to.be.a("string").and.not.be.empty;
     //expect(password, 'password was set').to.be.a('string').and.not.be.empty - Avoid this line, you will see the password in videos/ss
-    if (typeof password !== 'string' || !password) {
-      throw new Error('Missing password value, set using CYPRESS_password=...')
+    if (typeof password !== "string" || !password) {
+      throw new Error("Missing password value, set using CYPRESS_password=...");
     }
 
     cy.safeLogin(username, password);
--- cypress/e2e/41-cy.origin.cy.ts
+++ cypress/e2e/41-cy.origin.cy.ts
@@ -2,15 +2,15 @@
 //The website https://mocklab-demo.herokuapp.com/login?type=mock is live at the momment I recorded this video
 //Since heroku won't be free anymore, the website may be down in the future.
 
-describe('origin demo', ()=>{
-    it('Testing a fake login', () => {
-        cy.visit('https://mocklab-demo.herokuapp.com/login?type=mock')
-        cy.get('.demo-box a').click()
-        cy.origin('https://mocklab.io', ()=>{
-            cy.get('[name="email"]').type('test@test.com')
-            cy.get('[name="password"]').type('fakePassword')
-            cy.get('#login-submit').click()
-        })
-        cy.get('h1 strong').should('have.text', 'test@test.com')
+describe("origin demo", () => {
+  it("Testing a fake login", () => {
+    cy.visit("https://mocklab-demo.herokuapp.com/login?type=mock");
+    cy.get(".demo-box a").click();
+    cy.origin("https://mocklab.io", () => {
+      cy.get('[name="email"]').type("test@test.com");
+      cy.get('[name="password"]').type("fakePassword");
+      cy.get("#login-submit").click();
     });
-})
+    cy.get("h1 strong").should("have.text", "test@test.com");
+  });
+});
