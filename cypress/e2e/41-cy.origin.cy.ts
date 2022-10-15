fb8456e035af88b08162ad232bf1df09e0e9a44b
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-10-14 21:19:45 -0600

    10.10 update

--- cypress/e2e/41-cy.origin.cy.ts
+++ cypress/e2e/41-cy.origin.cy.ts
@@ -2,11 +2,11 @@
 //The website https://mocklab-demo.herokuapp.com/login?type=mock is live at the momment I recorded this video
 //Since heroku won't be free anymore, the website may be down in the future.
 
-describe("origin demo", () => {
+describe.skip("origin demo - demo website is not working property. You can test it manually.", () => {
   it("Testing a fake login", () => {
-    cy.visit("https://mocklab-demo.herokuapp.com/login?type=mock");
-    cy.get(".demo-box a").click();
-    cy.origin("https://mocklab.io", () => {
+    cy.visit("https://mocklab-demo.herokuapp.com/login");
+    cy.get("div.demo-box a").click();
+    cy.origin("https://oauth.mocklab.io", () => {
       cy.get('[name="email"]').type("test@test.com");
       cy.get('[name="password"]').type("fakePassword");
       cy.get("#login-submit").click();
